cb8479049271a7c3b1c7980c9a5a3a15efdf0f1f
Nirav1210 <nirav.bhut1210@gmail.com>
2021-01-30 17:37:29 -0500

    add CRUD methods

--- tm-api/src/task/task.service.ts
+++ tm-api/src/task/task.service.ts
@@ -1,13 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateTaskDTO } from 'src/dto/create-task.dto';
 import { TaskDTO } from 'src/dto/task.dto';
+import { UpdateTaskDTO } from 'src/dto/update-task.dto';
 import { Task, TaskStatus } from 'src/entity/task.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class TaskService {
     constructor(@InjectRepository(Task) private taskRepository: Repository<Task>) {}
+
+    private entityToDTO(task: Task) {
+        const taskDTO = new TaskDTO();
+        taskDTO.id = task.id;
+        taskDTO.title = task.title;
+        taskDTO.description = task.description;
+        taskDTO.status = task.status;
+
+        return taskDTO;
+    }
+
+    public async getOne(taskId: number) {
+        const task: Task = await this.taskRepository.findOne(taskId);
+
+        if(!task) throw new NotFoundException("task not found");
+
+        const taskDTO: TaskDTO = this.entityToDTO(task);
+
+        return taskDTO;
+    }
 
     public async createOne(createTaskRequest: CreateTaskDTO) {
         const task: Task = new Task();
@@ -16,13 +37,32 @@ export class TaskService {
         task.status = TaskStatus.Created;
 
         await this.taskRepository.save(task);
-
-        const taskDTO = new TaskDTO();
-        taskDTO.id = task.id;
-        taskDTO.title = task.title;
-        taskDTO.description = task.description;
-        taskDTO.status = task.status;
+        const taskDTO: TaskDTO = this.entityToDTO(task);
 
         return taskDTO;
+    }
+
+    public async getAll() {
+        const tasks: Task[] = await this.taskRepository.find();
+        const tasksDTO: TaskDTO[] = tasks.map(task => this.entityToDTO(task));
+
+        return tasksDTO;
+    }
+
+    public async updateOne(taskId: number, updateTaskRequest: UpdateTaskDTO) {
+        const task: Task = await this.getOne(taskId);
+        task.title = updateTaskRequest.title || task.title;
+        task.description = updateTaskRequest.description || task.description;
+        task.status = updateTaskRequest.status || task.status;
+
+        await this.taskRepository.save(task);
+        const taskDTO: TaskDTO = this.entityToDTO(task);
+
+        return taskDTO;
+    }
+
+    public async deleteOne(taskId: number) {
+        const task: Task = await this.getOne(taskId);
+        await this.taskRepository.remove(task);
     }
 }
